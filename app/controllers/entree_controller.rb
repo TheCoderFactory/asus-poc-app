e2e38f9738f597d8a99d4b00d04cdec7477db766
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-04-13 15:02:27 +1000

    entree controller

---
+++ app/controllers/entree_controller.rb
@@ -0,0 +1,23 @@
+class EntreeController < ApplicationController
+  def index
+    @entrants = Entree.all
+  end
+
+  def new
+    @entrant = Entree.new
+  end
+
+  def create
+    @entrant = Entree.new(entree_params)
+    if @entrannt.save
+      redirect_to root
+    else
+      respond_with @entrant
+    end
+  end
+
+  private
+  def entree_params
+    params.require(:entree).permit(:first_name, :last_name, :email, :phone)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'entree/new'
+
+  get 'entree/create'
+
+  get 'entree/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
