ebee779f144e16345e6e1f09bda14e3a53162cdb
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-04-13 15:58:32 +1000

    new entrant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'entree/new'
 
-  get 'entree/create'
+  post 'entree/create'
 
   get 'entree/index'
+  resources :entrees
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
